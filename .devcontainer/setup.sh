8d794780c6d397f4abbbad43558c2185b6bb29b1
AHMED BELGANI <77440761+AHMEDBEL529@users.noreply.github.com>
2023-10-23 21:31:12 +0100

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Create the directory for models
-mkdir -p /workspaces/etl-ner-sentiment-analysis/models/
+mkdir -p /workspaces/etl-ner-sentiment-analysis/models/bert-base-NER
+mkdir -p /workspaces/etl-ner-sentiment-analysis/models/bertweet-base-sentiment-analysis
 
 # Clone the necessary repositories
-git clone https://huggingface.co/dslim/bert-base-NER /workspaces/etl-ner-sentiment-analysis/models/
-git clone https://huggingface.co/finiteautomata/bertweet-base-sentiment-analysis /workspaces/etl-ner-sentiment-analysis/models/
+git clone https://huggingface.co/dslim/bert-base-NER /workspaces/etl-ner-sentiment-analysis/models/bert-base-NER
+git clone https://huggingface.co/finiteautomata/bertweet-base-sentiment-analysis /workspaces/etl-ner-sentiment-analysis/models/bertweet-base-sentiment-analysis
 
 # Start the Astronomer development environment
 astro dev start -n --wait 8m
