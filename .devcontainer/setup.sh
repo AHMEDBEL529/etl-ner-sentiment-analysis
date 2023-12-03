9e5d3c7ca98194c64e32ac5dda351096150eeb7e
AHMED BELGANI <77440761+AHMEDBEL529@users.noreply.github.com>
2023-12-03 14:28:07 +0100

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+# Install Astronomer CLI
+curl -sSL install.astronomer.io | sudo bash -s
+
 # Create the directory for models
-mkdir -p /workspaces/etl-ner-sentiment-analysis/models/bert-base-NER
-mkdir -p /workspaces/etl-ner-sentiment-analysis/models/bertweet-base-sentiment-analysis
+mkdir -p ./models/bert-base-NER
+mkdir -p ./models/bertweet-base-sentiment-analysis
 
 # Clone the necessary repositories
-git clone https://huggingface.co/dslim/bert-base-NER /workspaces/etl-ner-sentiment-analysis/models/bert-base-NER
-git clone https://huggingface.co/finiteautomata/bertweet-base-sentiment-analysis /workspaces/etl-ner-sentiment-analysis/models/bertweet-base-sentiment-analysis
+git clone https://huggingface.co/dslim/bert-base-NER ./models/bert-base-NER
+git clone https://huggingface.co/finiteautomata/bertweet-base-sentiment-analysis ./models/bertweet-base-sentiment-analysis
+
+# Wait for Docker daemon to be ready
+sleep 20
 
 # Start the Astronomer development environment
 astro dev start -n --wait 8m
