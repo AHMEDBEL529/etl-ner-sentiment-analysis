1e0a659236bcbdb0d55bb3bb4aa053bc15dc7cf4
AHMED BELGANI <77440761+AHMEDBEL529@users.noreply.github.com>
2023-12-25 15:36:50 +0100

    Update nlp_inference.py

--- include/nlp_inference.py
+++ include/nlp_inference.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import duckdb
 from transformers import pipeline
-from transformers import BertForTokenClassification, BertTokenizer, TokenClassificationPipeline
 from include.global_variables import global_variables as gv
+import spacy
+from googleapiclient.discovery import build
+from textblob import TextBlob
+from span_marker import SpanMarkerModel
 
-pretrained_sentiment = "/usr/local/airflow/models/bertweet-base-sentiment-analysis"
-pretrained_ner = "/usr/local/airflow/models/bert-base-NER"
+
 duck_db_instance_name = (
     "dwh"  # when changing this value also change the db name in .env
 )
@@ -32,104 +34,72 @@ def get_comments_data(db=f"/usr/local/airflow/{duck_db_instance_name}"):
 
     return df
 
-class ModelLoader:
-    _sentiment_model = None
-    _ner_model = None
-
-    @classmethod
-    def get_sentiment_model(cls):
-        if cls._sentiment_model is None:
-            cls._sentiment_model = pipeline(
-                "sentiment-analysis",
-                model=pretrained_sentiment,
-                tokenizer=pretrained_sentiment,
-                top_k=None  # replace return_all_scores=True with this
-            )
-        return cls._sentiment_model
-
-    @classmethod
-    def get_ner_model(cls):
-        if cls._ner_model is None:
-            model = BertForTokenClassification.from_pretrained(pretrained_ner)
-            tokenizer = BertTokenizer.from_pretrained(pretrained_ner)
-            cls._ner_model = TokenClassificationPipeline(
-                model=model,
-                tokenizer=tokenizer,
-                aggregation_strategy="simple"  # replace grouped_entities=True with this
-            )
-        return cls._ner_model
 
 
+pretrained_ner = "/usr/local/airflow/models/span-marker-mbert-base-multinerd"
+ner_model = SpanMarkerModel.from_pretrained(pretrained_ner)
 
-def sentiment_analysis(text):
-    model = ModelLoader.get_sentiment_model()
-    output = model(text)
-    result = {elm["label"]: elm["score"] for elm in output[0]}
-    return max(result, key=result.get)
+# Function to extract 'PER' entities from the 'entities' column
+def extract_per_entities(entities):
+    return [entity['span'] for entity in entities if entity['label'] == 'PER']
 
-def ner(text):
-    model = ModelLoader.get_ner_model()
-    entities = model(text)
-    # Convert any numpy objects to native Python types here
-    return [{k: (v.tolist() if hasattr(v, 'tolist') else v) for k, v in entity.items()} for entity in entities]
-
-
-# Assuming you have the function definitions for sentiment_analysis and ner
-# from your original code
-
-def process_data(row):
+def ner_and_extract_per(text, model):
     try:
-        text = row['text'][:128]  # Truncate text
-        return pd.Series([text, sentiment_analysis(text), ner(text)])
+        entities = model.predict(text)
+        per_entities = extract_per_entities(entities)
+        return per_entities
     except Exception as e:
-        # handle or log exception
-        return pd.Series([None, None, None])
-        print(f"exception:{e}")
-    
-import spacy
-from googleapiclient.discovery import build
-from textblob import TextBlob
+        print(f"Exception in NER processing: {e}")
+        return []
+
+
+pretrained_sentiment = "/usr/local/airflow/models/bart-large-mnli"
+sentiment_model = pipeline(
+    "zero-shot-classification",
+    model=pretrained_sentiment,
+    return_all_scores=True
+)
+
+def zero_shot_sentiment_analysis(row, model):
+  if not row['entities']:
+    return {}
+  candidate_labels = ["positive", "negative", "neutral"]
+  result = {}
+  for entity in row['entities'] :
+    hypothesis_template = f"the sentiment towards {entity} is {{}}."
+    output = model([row['comment']], candidate_labels, hypothesis_template=hypothesis_template)
+
+    # Extract the first element of the list (assuming it contains the most relevant results)
+    top_result = output[0]
+
+    # Convert the list of dictionaries to a single dictionary
+    label_score = {label: score for label, score in zip(top_result['labels'], top_result['scores'])}
+
+    result[entity] = max(label_score, key=label_score.get)
+
+  return result
+
 
 
 def main():
-    print('testing model')
-    print(sentiment_analysis('Jacob, Youssef, sad'))
-    print(ner('Jacob, Youssef, sad'))
-    # comments = get_comments(VIDEO_ID)
-    df = get_comments_data(db=f"/usr/local/airflow/{duck_db_instance_name}")
-    comments = df['TEXT'].tolist()
+    df_comments = get_comments_data(db=f"/usr/local/airflow/{duck_db_instance_name}")
+    comments = df_comments['TEXT'].tolist()
 
     # Assuming comments is a list. Convert to DataFrame
-    df = pd.DataFrame(comments[:9000], columns=['text'])
+    df = pd.DataFrame(comments[:9000], columns=['comment'])
     # print(df)
     # Get the number of rows in the DataFrame
     num_rows = len(df)
     print(f"number of rows : {num_rows}")
-    # Process data
-    processed_df = df.apply(process_data, axis=1)
-    processed_df.columns = ["text", "sentiment", "entities"]
 
-    exploded_df = processed_df.explode('entities').rename(columns={'entities': 'entity'})
-    # print(exploded_df)
-    # Filter where entity group is "PER" and get the corresponding names
-    names_df = exploded_df[exploded_df['entity'].apply(lambda x: x['entity_group'] if isinstance(x, dict) and 'entity_group' in x else None) == 'PER']
-    names_df = names_df.copy()
-    names_df.loc[:, 'name'] = names_df['entity'].apply(lambda x: x['word'] if isinstance(x, dict) and 'word' in x else None)
-
-    # Drop the entity column as it's no longer needed
-    names_df = names_df.drop('entity', axis=1)
-
-    # Group by text and sentiment to get list of names for each text
-    result_df = names_df.groupby(['text', 'sentiment'])['name'].apply(list).reset_index()
-
-    # Explode the names column along with sentiment
-    exploded_df = result_df.explode('name')
-
-    # Remove non-alphabetical characters and convert to lowercase
-    exploded_df['name'] = exploded_df['name'].str.replace("[^a-zA-Z]", "").str.lower()
-
-    # Group by 'name' and 'sentiment' and count the occurrences
-    sentiment_counts = exploded_df.groupby(['name', 'sentiment']).size().reset_index(name='count')
+    df['entities'] = df['comment'].apply(lambda x: ner_and_extract_per(x, ner_model))
+    df['ner_sentiment_analysis'] = df.apply(lambda row: zero_shot_sentiment_analysis(row, sentiment_model), axis=1)
+    df = df[df['ner_sentiment_analysis'].notna() & (df['ner_sentiment_analysis'] != {})]
+    flattened_data = pd.DataFrame([(entity, sentiment) for entities in df['ner_sentiment_analysis'] for entity, sentiment in entities.items()], columns=['name', 'sentiment'])
+    sentiment_counts = flattened_data.groupby(['name', 'sentiment']).size().reset_index(name='count')
+    
+    # Display the grouped data
+    print(sentiment_counts)
 
     # connect to an in-memory database
     con = duckdb.connect(f"/usr/local/airflow/{duck_db_instance_name}")
